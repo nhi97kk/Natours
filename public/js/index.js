9f2a593c352b0df8fb6d4e3e96b0b837993a0017
VÕ THỊ YẾN NHI <yennhi20vl@gmail.com>
2024-03-15 15:06:02 +0700

    "after 1 month started"

---
+++ public/js/index.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+// import { login } from './login';
+// import axios from 'axios';
+
+//alert
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
+
+// type is 'success' or 'error'
+const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
+};
+
+//dom elements
+const loginForm = document.querySelector('.form');
+
+if (loginForm)
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+
+const login = async (email, password) => {
+  console.log(email, password);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password
+      }
+    });
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
---
+++ routes/viewRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const viewController = require('../controllers/viewController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.get('/', authController.isLoggedIn, viewController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
+router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
+
+module.exports = router;
